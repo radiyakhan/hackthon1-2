8db6a4a0f20400c4bbd669fb5a9ebb8c23ff2726
radiyakhan <radiyakhan59@gmail.com>
2024-09-08 16:08:13 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var toggleButton = document.getElementById("toggle-skills");
+    var skillsSection = document.getElementById("skills");
+    toggleButton.addEventListener('click', function () {
+        if (skillsSection.style.display === 'none') {
+            skillsSection.style.display = 'block';
+        }
+        else {
+            skillsSection.style.display = 'none';
+        }
+    });
+});
